3cf0cfc5893c97c117136aac8532ada95cde4da3
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-03-06 20:21:50 +0000

    update version.

--- Pods/CryptoSwift/Sources/CryptoSwift/AES.Cryptors.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/AES.Cryptors.swift
@@ -9,4 +9,26 @@
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
 //  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
-//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original softw
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+// MARK: Cryptors
+
+extension AES: Cryptors {
+    public func makeEncryptor() throws -> AES.Encryptor {
+        return try AES.Encryptor(aes: self)
+    }
+
+    public func makeDecryptor() throws -> AES.Decryptor {
+        return try AES.Decryptor(aes: self)
+    }
+}
+
+// MARK: Encryptor
+
+extension AES {
+    public struct Encryptor: Updatable {
+        private var worker: BlockModeWorker
+        private let padding: Padding
+        private var 
