613723a3a8b465cfcf157e3a2149670429f61480
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-03-27 22:18:31 +0000

    More clean up.

--- Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/BlockModeWorker.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/BlockModeWorker.swift
@@ -15,4 +15,6 @@
 
 protocol BlockModeWorker {
     var cipherOperation: CipherOperationOnBlock { get }
-    mutating func encrypt(_ plaintext: ArraySlice
+    mutating func encrypt(_ plaintext: ArraySlice<UInt8>) -> Array<UInt8>
+    mutating func decrypt(_ ciphertext: ArraySlice<UInt8>) -> Array<UInt8>
+}
