400193d598150e5efccda7bb43c85ce0b5159af2
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-04-04 18:31:52 +0000

    SERVER_JEKYLL_GENERATE_FROM_SERVER

--- Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/CTR.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/CTR.swift
@@ -8,4 +8,17 @@
 //
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
-//  - The origin of this software must not be misrepresented; you must not cl
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+//  Counter (CTR)
+//
+
+struct CTRModeWorker: RandomAccessBlockModeWorker {
+    let cipherOperation: CipherOperationOnBlock
+    private let iv: ArraySlice<UInt8>
+    var counter: UInt = 0
+
+    init(iv: Ar
