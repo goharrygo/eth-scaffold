6144eefc13fb6ff1dbc819661f4e542596ee2d62
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-04-04 14:19:13 +0000

    rpc

--- Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/CFB.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/CFB.swift
@@ -9,4 +9,22 @@
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
 //  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
-//  - Altered source versions must be plainly marked as such, and must not be misrepresented 
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+//  Cipher feedback (CFB)
+//
+
+struct CFBModeWorker: BlockModeWorker {
+    let cipherOperation: CipherOperationOnBlock
+    private let iv: ArraySlice<UInt8>
+    private var prev: ArraySlice<UInt8>?
+
+    init(iv: ArraySlice<UInt8>, cipherOperation: @escaping CipherOperationOnBlock) {
+        self.iv = iv
+        self.cipherOperation = cipherOperation
+    }
+
+    mutating func encrypt(_ plaintext: ArraySlice<UInt8>) -> Array<UInt8> {
+        guard let ciphe
