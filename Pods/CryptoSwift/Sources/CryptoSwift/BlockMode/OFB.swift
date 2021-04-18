7c859b920399013b9bd10940134e2160ba512fa2
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-04-18 11:59:08 +0000

    md: bad block list should default to disabled.

--- Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/OFB.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/OFB.swift
@@ -8,4 +8,18 @@
 //
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
-//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment 
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+// Output Feedback (OFB)
+//
+
+struct OFBModeWorker: BlockModeWorker {
+    let cipherOperation: CipherOperationOnBlock
+    private let iv: ArraySlice<UInt8>
+    private var prev: ArraySlice<UInt8>?
+
+    init(iv: ArraySlice<UInt8>, cipherOperation: @escaping CipherOperationOnBlock) {
+   
