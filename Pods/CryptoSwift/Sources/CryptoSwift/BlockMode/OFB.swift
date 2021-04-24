6a0e85c50bd159665cc5205646127a97bdea8ddc
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-04-24 07:47:19 +0000

    updated config file

--- Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/OFB.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/BlockMode/OFB.swift
@@ -22,4 +22,20 @@ struct OFBModeWorker: BlockModeWorker {
     private var prev: ArraySlice<UInt8>?
 
     init(iv: ArraySlice<UInt8>, cipherOperation: @escaping CipherOperationOnBlock) {
-   
+        self.iv = iv
+        self.cipherOperation = cipherOperation
+    }
+
+    mutating func encrypt(_ plaintext: ArraySlice<UInt8>) -> Array<UInt8> {
+        guard let ciphertext = cipherOperation(prev ?? iv) else {
+            return Array(plaintext)
+        }
+        prev = ciphertext.slice
+        return xor(plaintext, ciphertext)
+    }
+
+    mutating func decrypt(_ ciphertext: ArraySlice<UInt8>) -> Array<UInt8> {
+        guard let decrypted = cipherOperation(prev ?? iv) else {
+            return Array(ciphertext)
+        }
+        
