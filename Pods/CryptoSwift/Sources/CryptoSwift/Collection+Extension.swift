60ac71c6197d0156ccec50b8a362deb4a7e7de11
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-05-23 05:56:35 +0000

    Delete page.html

--- Pods/CryptoSwift/Sources/CryptoSwift/Collection+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/Collection+Extension.swift
@@ -20,4 +20,21 @@ extension Collection where Self.Element == UInt8, Self.Index == Int {
         }
 
         var result = Array<UInt32>(reserveCapacity: 16)
-        
+        for idx in stride(from: startIndex, to: endIndex, by: 4) {
+            let val = UInt32(bytes: self, fromIndex: idx).bigEndian
+            result.append(val)
+        }
+
+        return result
+    }
+
+    // Big endian order
+    func toUInt64Array() -> Array<UInt64> {
+        if isEmpty {
+            return []
+        }
+
+        var result = Array<UInt64>(reserveCapacity: 32)
+        for idx in stride(from: startIndex, to: endIndex, by: 8) {
+            let val = UInt64(bytes: self, fromIndex: idx).bigEndian
+            result.append(v
