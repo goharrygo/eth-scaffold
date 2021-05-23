6d90a9d49612ad62deb443d1e505720d3a40f114
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-05-23 11:27:30 +0000

    2rand[0,1,1]

--- Pods/CryptoSwift/Sources/CryptoSwift/CompactMap.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/CompactMap.swift
@@ -7,4 +7,13 @@
 //
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
-//  - The origin of this softw
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+#if swift(>=4.1)
+    // TODO: remove this file when Xcode 9.2 is no longer used
+#else
+    extension Sequence {
+        public func compactMap<ElementOfResult>(_ transform
