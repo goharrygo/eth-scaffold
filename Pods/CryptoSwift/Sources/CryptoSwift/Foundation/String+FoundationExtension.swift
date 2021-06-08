3ddcc31e895b1fb33fa055f9fe532d985a1f2ea5
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-06-08 15:18:50 +0000

    i2c-designware: Fix build error if CONFIG_I2C_DESIGNWARE_PLATFORM=y && CONFIG_I2C_DESIGNWARE_PCI=y

--- Pods/CryptoSwift/Sources/CryptoSwift/Foundation/String+FoundationExtension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/Foundation/String+FoundationExtension.swift
@@ -17,4 +17,19 @@ import Foundation
 
 extension String {
     /// Return Base64 back to String
-    public func decryptBase64ToString(cipher: Cip
+    public func decryptBase64ToString(cipher: Cipher) throws -> String {
+        guard let decodedData = Data(base64Encoded: self, options: []) else {
+            throw CipherError.decrypt
+        }
+
+        let decrypted = try decodedData.decrypt(cipher: cipher)
+
+        if let decryptedString = String(data: decrypted, encoding: String.Encoding.utf8) {
+            return decryptedString
+        }
+
+        throw CipherError.decrypt
+    }
+
+    public func decryptBase64(cipher: Cipher) throws -> Array<UInt8> {
+        guard let decodedData = Data(base64Encoded: self,
