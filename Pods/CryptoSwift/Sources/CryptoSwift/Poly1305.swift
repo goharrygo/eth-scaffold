41cf62a8b9c29806e5db90d0f63b1d4c1ccc82b1
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-07-26 14:00:27 +0000

    identifying user matches

--- Pods/CryptoSwift/Sources/CryptoSwift/Poly1305.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/Poly1305.swift
@@ -9,4 +9,14 @@
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
 //  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
 //
+
+//  http://tools.ietf.org/html/draft-agl-tls-chacha20poly1305-04#section-4
+//  nacl/crypto_onetimeauth/poly1305/ref/auth.c
+//
+///  Poly1305 takes a 32-byte, one-time key and a message and produces a 16-byte tag that authenticates the
+///  message such that an attacker has a negligible chance of producing a valid tag for an inauthentic message.
+
+publi
