59c8681b17831d548cdb81f4253792032f75f3ae
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-08-21 19:31:14 +0000

    add full license text

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
@@ -8,4 +8,18 @@
 //
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
-//  - The origin of this software must not be misrepresented; you must not claim that 
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+#if os(Linux) || os(Android) || os(FreeBSD)
+    import Glibc
+#else
+    import Darwin
+#endif
+
+protocol _UInt32Type {}
+extension UInt32: _UInt32Type {}
+
+/** a
