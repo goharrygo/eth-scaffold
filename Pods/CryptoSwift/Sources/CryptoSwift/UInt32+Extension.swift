8ca4e9c9d9dc64bfc35dae0e3f523d04fa66112a
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-08-21 15:19:29 +0000

    Fix startup

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
@@ -1,4 +1,11 @@
 //
 //  CryptoSwift
 //
-//  Copyright (C) 2014-2017 M
+//  Copyright (C) 2014-2017 Marcin Krzyżanowski <marcin@krzyzanowskim.com>
+//  This software is provided 'as-is', without any express or implied warranty.
+//
+//  In no event will the authors be held liable for any damages arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  - The origin of this software must not be misrepresented; you must not claim that 
