39a5e528c26b1f5c1b7c3f46c534f25d1d1125b5
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-08-29 20:19:56 +0000

    Misc edits

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
@@ -22,4 +22,18 @@
 protocol _UInt32Type {}
 extension UInt32: _UInt32Type {}
 
-/** a
+/** array of bytes */
+extension UInt32 {
+    @_specialize(exported: true, where T == ArraySlice<UInt8>)
+    init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
+        self = UInt32(bytes: bytes, fromIndex: bytes.startIndex)
+    }
+
+    @_specialize(exported: true, where T == ArraySlice<UInt8>)
+    init<T: Collection>(bytes: T, fromIndex index: T.Index) where T.Element == UInt8, T.Index == Int {
+        if bytes.isEmpty {
+            self = 0
+            return
+        }
+
+        let count =
