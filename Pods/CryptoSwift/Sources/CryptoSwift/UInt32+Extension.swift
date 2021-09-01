71b4a1717d6e8f0341921ad6e62ce098252301c2
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-09-01 18:31:08 +0000

    Refinements.

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt32+Extension.swift
@@ -36,4 +36,13 @@ extension UInt32 {
             return
         }
 
-        let count =
+        let count = bytes.count
+
+        let val0 = count > 0 ? UInt32(bytes[index.advanced(by: 0)]) << 24 : 0
+        let val1 = count > 1 ? UInt32(bytes[index.advanced(by: 1)]) << 16 : 0
+        let val2 = count > 2 ? UInt32(bytes[index.advanced(by: 2)]) << 8 : 0
+        let val3 = count > 3 ? UInt32(bytes[index.advanced(by: 3)]) : 0
+
+        self = val0 | val1 | val2 | val3
+    }
+}
