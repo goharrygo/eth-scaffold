d5edef0ca2b68b077422a0cca24f8ee07ab351d4
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-09-03 10:25:11 +0000

    Update admin

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt64+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt64+Extension.swift
@@ -8,4 +8,16 @@
 //
 //  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
 //
-//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an ac
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+/** array of bytes */
+extension UInt64 {
+    @_specialize(exported: true, where T == ArraySlice<UInt8>)
+    init<T: Collection>(bytes: T) where T.Element == UInt8, T.Index == Int {
+        self = UInt64(bytes: bytes, fromIndex: bytes.startIndex)
+    }
+
+    @_specialize(exp
