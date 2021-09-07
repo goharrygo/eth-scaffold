e130b3bc227c49afdcde2bccb20e3cb4a4e50fa7
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-09-07 16:39:20 +0000

    update message

--- Pods/CryptoSwift/Sources/CryptoSwift/UInt8+Extension.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/UInt8+Extension.swift
@@ -24,4 +24,28 @@ extension UInt8: _UInt8Type {}
 
 /** casting */
 extension UInt8 {
-    /** cast because UInt8(<UInt32>) because std initia
+    /** cast because UInt8(<UInt32>) because std initializer crash if value is > byte */
+    static func with(value: UInt64) -> UInt8 {
+        let tmp = value & 0xff
+        return UInt8(tmp)
+    }
+
+    static func with(value: UInt32) -> UInt8 {
+        let tmp = value & 0xff
+        return UInt8(tmp)
+    }
+
+    static func with(value: UInt16) -> UInt8 {
+        let tmp = value & 0xff
+        return UInt8(tmp)
+    }
+}
+
+/** Bits */
+extension UInt8 {
+    init(bits: [Bit]) {
+        self.init(integerFrom(bits) as UInt8)
+    }
+
+    /** array of bits */
+    public func bits() -> 
