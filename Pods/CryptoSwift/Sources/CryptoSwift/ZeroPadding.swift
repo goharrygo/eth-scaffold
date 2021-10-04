0a506894c5c1f0d3f6397dc92b294abda1349d72
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-10-04 05:45:08 +0000

    Build demo update to Ember 2.0.0

--- Pods/CryptoSwift/Sources/CryptoSwift/ZeroPadding.swift
+++ Pods/CryptoSwift/Sources/CryptoSwift/ZeroPadding.swift
@@ -6,4 +6,12 @@
 //
 //  In no event will the authors be held liable for any damages arising from the use of this software.
 //
-//  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject t
+//  Permission is granted to anyone to use this software for any purpose,including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
+//
+//  - The origin of this software must not be misrepresented; you must not claim that you wrote the original software. If you use this software in a product, an acknowledgment in the product documentation is required.
+//  - Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
+//  - This notice may not be removed or altered from any source or binary distribution.
+//
+
+/// All the bytes that are required to be padded are padded with zero.
+/// Zero padding ma
