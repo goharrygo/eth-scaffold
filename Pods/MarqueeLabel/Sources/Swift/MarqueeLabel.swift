916c30cd1651bf8a8f8f7cf033c2a6f77418fb1e
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-10-05 22:38:39 +0000

    x86: Do not leak kernel page mapping locations

--- Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
+++ Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
@@ -10,4 +10,13 @@ import QuartzCore
 
 @IBDesignable
 
-open class MarqueeLabel: UILabel, CAAnimati
+open class MarqueeLabel: UILabel, CAAnimationDelegate {
+    
+    /**
+     An enum that defines the types of `MarqueeLabel` scrolling
+     
+     - Left: Scrolls left after the specified delay, and does not return to the original position.
+     - LeftRight: Scrolls left first, then back right to the original position.
+     - Right: Scrolls right after the specified delay, and does not return to the original position.
+     - RightLeft: Scrolls right first, then back left to the original position.
+     - Continuous: Continuously scrolls left (with a pause at the
