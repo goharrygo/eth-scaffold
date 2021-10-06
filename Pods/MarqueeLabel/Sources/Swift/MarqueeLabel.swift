6b10899e214dddb59d4f19489cba4efd752c68dc
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-10-06 14:28:23 +0000

    fix translation

--- Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
+++ Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
@@ -19,4 +19,22 @@ open class MarqueeLabel: UILabel, CAAnimationDelegate {
      - LeftRight: Scrolls left first, then back right to the original position.
      - Right: Scrolls right after the specified delay, and does not return to the original position.
      - RightLeft: Scrolls right first, then back left to the original position.
-     - Continuous: Continuously scrolls left (with a pause at the
+     - Continuous: Continuously scrolls left (with a pause at the original position if animationDelay is set).
+     - ContinuousReverse: Continuously scrolls right (with a pause at the original position if animationDelay is set).
+     */
+    public enum MarqueeType {
+        case left
+        case leftRight
+        case right
+        case rightLeft
+        case continuous
+        case continuousReverse
+    }
+    
+    //
+    // MARK: - Public properties
+    //
+    
+    /**
+     Defines the direction and method in which the `MarqueeLabel` instance scrolls.
+     `MarqueeLabel` supports six default types of scr
