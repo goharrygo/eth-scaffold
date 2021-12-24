421e1dc2a9482449285e9c4b45e2f83aa83376f7
goharrygo <109819723+goharrygo@users.noreply.github.com>
2021-12-24 15:23:43 +0000

    rebuild linux-davinci from branches

--- Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
+++ Pods/MarqueeLabel/Sources/Swift/MarqueeLabel.swift
@@ -682,4 +682,10 @@ open class MarqueeLabel: UILabel, CAAnimationDelegate {
                     ScrollStep(timeStep: animationDuration, timingFunction: animationCurve,         // Away position, using animationCurve transition, with only leading edge faded in
                         position: .away, edgeFades: .leading),
                     ScrollStep(timeStep: animationDelay, position: .away, edgeFades: .leading),     // Delay at away, maintaining fade state (leading only)
-                    FadeStep(timeStep: 0.2, edgeFades: [.leading, .trailing]),                      // 0.2 sec afte
+                    FadeStep(timeStep: 0.2, edgeFades: [.leading, .trailing]),                      // 0.2 sec after delay ends, fade trailing edge back in as well
+                    FadeStep(timeStep: -0.2, edgeFades: [.leading, .trailing]),                     // Maintain fade state until 0.2 sec before reaching home position
+                    ScrollStep(timeStep: animationDuration, timingFunction: animationCurve,         // Ending point, back at home, with only trailing fade
+                        position: .home, edgeFades: .trailing)
+                ]
+            } else { // .left or .right
+                sequence 
