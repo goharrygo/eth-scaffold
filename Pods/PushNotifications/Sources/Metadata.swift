cd5a1235d53c19a3ecd9e7d5eb29eb453d072406
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-05-23 12:46:15 +0000

    Small README fix

--- Pods/PushNotifications/Sources/Metadata.swift
+++ Pods/PushNotifications/Sources/Metadata.swift
@@ -13,4 +13,19 @@ extension Metadata: PropertyListReadable {
 
     init(propertyListRepresentation: [String: Any]) {
         self.sdkVersion = propertyListRepresentation["sdkVersion"]  as? String
-        self.iosVersion = 
+        self.iosVersion = propertyListRepresentation["iosVersion"] as? String
+        self.macosVersion = propertyListRepresentation["macosVersion"] as? String
+    }
+
+    func hasChanged() -> Bool {
+        return self.sdkVersion != SDK.version
+    }
+
+    static func update() -> Metadata {
+        let sdkVersion = SDK.version
+        let systemVersion = SystemVersion.version
+
+        #if os(iOS)
+            let metadata = Metadata(sdkVersion: sdkVersion, iosVersion: systemVersion, macosVersion: nil)
+        #elseif os(OSX)
+            let metadata = 
