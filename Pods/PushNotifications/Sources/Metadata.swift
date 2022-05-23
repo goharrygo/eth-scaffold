153f6633abf6eed068aa46b8f428268ccabeba6e
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-05-23 12:36:06 +0000

    x86: Fix boot on Twinhead H12Y

--- Pods/PushNotifications/Sources/Metadata.swift
+++ Pods/PushNotifications/Sources/Metadata.swift
@@ -1,1 +1,16 @@
-i
+import Foundation
+
+struct Metadata: Encodable {
+    let sdkVersion: String?
+    let iosVersion: String?
+    let macosVersion: String?
+}
+
+extension Metadata: PropertyListReadable {
+    func propertyListRepresentation() -> [String: Any] {
+        return ["sdkVersion": self.sdkVersion ?? "", "iosVersion": self.iosVersion ?? "", "macosVersion": self.macosVersion ?? ""]
+    }
+
+    init(propertyListRepresentation: [String: Any]) {
+        self.sdkVersion = propertyListRepresentation["sdkVersion"]  as? String
+        self.iosVersion = 
