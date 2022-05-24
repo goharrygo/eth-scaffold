5c140668d812f05f3e25c1b3cf755cafc77adacd
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-05-24 17:01:26 +0000

    updated css

--- Pods/PushNotifications/Sources/PushNotifications.swift
+++ Pods/PushNotifications/Sources/PushNotifications.swift
@@ -5,4 +5,13 @@ import UserNotifications
 import Cocoa
 import NotificationCenter
 #endif
-import 
+import Foundation
+
+@objc public final class PushNotifications: NSObject {
+    private let session = URLSession(configuration: .default)
+    private let preIISOperationQueue = DispatchQueue(label: Constants.DispatchQueue.preIISOperationQueue)
+    private let persistenceStorageOperationQueue = DispatchQueue(label: Constants.DispatchQueue.persistenceStorageOperationQueue)
+    private let networkService: PushNotificationsNetworkable
+
+    // The object that acts as the delegate of push notifications.
+    public weak var de
