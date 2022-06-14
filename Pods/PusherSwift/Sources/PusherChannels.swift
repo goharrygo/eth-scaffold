053804a1d23f8fb107a1ad80e2dd180bcd8aa5aa
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-06-14 05:49:58 +0000

    Fixes conflict

--- Pods/PusherSwift/Sources/PusherChannels.swift
+++ Pods/PusherSwift/Sources/PusherChannels.swift
@@ -9,4 +9,9 @@ import Foundation
         of channels
 
         - parameter name:            The name of the channel to create
-        - parameter connection
+        - parameter connection:      The connection associated with the channel being created
+        - parameter auth:            A PusherAuth value if subscription is being made to an
+                                     authenticated channel without using the default auth methods
+        - parameter onMemberAdded:   A function that will be called with information about the
+                                     member who has just joined the presence channel
+        - parameter onMemberRemoved: A function that will be called with infor
