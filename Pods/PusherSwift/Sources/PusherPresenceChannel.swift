5964df9ab4a08e530f1b33286279255d072519dc
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-06-28 21:39:16 +0000

    Reviewed by Brady.

--- Pods/PusherSwift/Sources/PusherPresenceChannel.swift
+++ Pods/PusherSwift/Sources/PusherPresenceChannel.swift
@@ -10,4 +10,12 @@ public typealias PusherUserInfoObject = [String : AnyObject]
     open var myId: String? = nil
 
     /**
-        Initializes a new PusherPresenceChannel with a given
+        Initializes a new PusherPresenceChannel with a given name, conenction, and optional
+        member added and member removed handler functions
+
+        - parameter name:            The name of the channel
+        - parameter connection:      The connection that this channel is relevant to
+        - parameter auth:            A PusherAuth value if subscription is being made to an
+                                     authenticated channel without using the default auth methods
+        - parameter onMemberAdded:   A function that will be called with information about the
+                 
