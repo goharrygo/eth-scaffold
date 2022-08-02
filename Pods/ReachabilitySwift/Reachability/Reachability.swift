fcbdee2e0074d4a9e1bccd04cfd3628b5ee96df7
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-08-02 18:30:05 +0000

    etc.

--- Pods/ReachabilitySwift/Reachability/Reachability.swift
+++ Pods/ReachabilitySwift/Reachability/Reachability.swift
@@ -55,4 +55,19 @@ public class Reachability {
     public typealias NetworkReachable = (Reachability) -> ()
     public typealias NetworkUnreachable = (Reachability) -> ()
 
-    @available(*, 
+    @available(*, unavailable, renamed: "Conection")
+    public enum NetworkStatus: CustomStringConvertible {
+        case notReachable, reachableViaWiFi, reachableViaWWAN
+        public var description: String {
+            switch self {
+            case .reachableViaWWAN: return "Cellular"
+            case .reachableViaWiFi: return "WiFi"
+            case .notReachable: return "No Connection"
+            }
+        }
+    }
+
+    public enum Connection: CustomStringConvertible {
+        case none, wifi, cellular
+        public var description: String {
+          
