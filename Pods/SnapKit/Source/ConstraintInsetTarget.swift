d1dc1836a05e724921054c56a533aee5531cf854
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-08-30 16:44:53 +0000

    second commit

--- Pods/SnapKit/Source/ConstraintInsetTarget.swift
+++ Pods/SnapKit/Source/ConstraintInsetTarget.swift
@@ -59,4 +59,14 @@ extension ConstraintInsetTarget {
         } else if let amount = self as? Double {
             return ConstraintInsets(top: CGFloat(amount), left: CGFloat(amount), bottom: CGFloat(amount), right: CGFloat(amount))
         } else if let amount = self as? CGFloat {
-            return ConstraintInsets(top: am
+            return ConstraintInsets(top: amount, left: amount, bottom: amount, right: amount)
+        } else if let amount = self as? Int {
+            return ConstraintInsets(top: CGFloat(amount), left: CGFloat(amount), bottom: CGFloat(amount), right: CGFloat(amount))
+        } else if let amount = self as? UInt {
+            return ConstraintInsets(top: CGFloat(amount), left: CGFloat(amount), bottom: CGFloat(amount), right: CGFloat(amount))
+        } else {
+            return ConstraintInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+    }
+    
+}
---
+++ Pods/SnapKit/Source/ConstraintInsets.swift
@@ -0,0 +1,4 @@
+//
+//  SnapKit
+//
+//  Copyrig
