92ff52a5f7973bf823e448a2486c3d2daf202a69
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-09-01 08:46:49 +0000

    Add verbose mode

---
+++ Pods/SnapKit/Source/ConstraintItem.swift
@@ -0,0 +1,8 @@
+//
+//  SnapKit
+//
+//  Copyright (c) 2011-Present SnapKit Team - https://github.com/SnapKit
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software
