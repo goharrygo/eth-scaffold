a1e93036ba1ca76aefd70735af3039387775ba02
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-09-03 11:20:40 +0000

    x86/efi: Enforce CONFIG_RELOCATABLE for EFI boot stub

--- Pods/SnapKit/Source/ConstraintItem.swift
+++ Pods/SnapKit/Source/ConstraintItem.swift
@@ -31,4 +31,24 @@
 public final class ConstraintItem {
     
     internal weak var target: AnyObject?
-    internal let attributes: Constrain
+    internal let attributes: ConstraintAttributes
+    
+    internal init(target: AnyObject?, attributes: ConstraintAttributes) {
+        self.target = target
+        self.attributes = attributes
+    }
+    
+    internal var layoutConstraintItem: LayoutConstraintItem? {
+        return self.target as? LayoutConstraintItem
+    }
+    
+}
+
+public func ==(lhs: ConstraintItem, rhs: ConstraintItem) -> Bool {
+    // pointer equality
+    guard lhs !== rhs else {
+        return true
+    }
+    
+    // must both have valid targets and identical attributes
+    guard let targ
