2772ffb9116ac545878dcd6dbd5c5df4da6da08a
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-09-17 10:57:30 +0000

    isdn/gigaset: fix CAPI disconnect B3 handling

--- Pods/SnapKit/Source/ConstraintMakerFinalizable.swift
+++ Pods/SnapKit/Source/ConstraintMakerFinalizable.swift
@@ -31,3 +31,19 @@
 public class ConstraintMakerFinalizable {
     
     internal let description: ConstraintDescription
+    
+    internal init(_ description: ConstraintDescription) {
+        self.description = description
+    }
+    
+    @discardableResult
+    public func labeled(_ label: String) -> ConstraintMakerFinalizable {
+        self.description.label = label
+        return self
+    }
+    
+    public var constraint: Constraint {
+        return self.description.constraint!
+    }
+    
+}
