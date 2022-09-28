f58b9ebb8771af571ba106d5ec401874c50f2a9d
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-09-28 16:09:46 +0000

    [nes]: ETHERNET changes ...

--- Pods/SnapKit/Source/ConstraintMakerRelatable.swift
+++ Pods/SnapKit/Source/ConstraintMakerRelatable.swift
@@ -86,4 +86,13 @@ public class ConstraintMakerRelatable {
     }
     
     @discardableResult
-    public func lessThanOrEqualTo(_ other: ConstraintRelatableTarget, _ file: String = #file
+    public func lessThanOrEqualTo(_ other: ConstraintRelatableTarget, _ file: String = #file, _ line: UInt = #line) -> ConstraintMakerEditable {
+        return self.relatedTo(other, relation: .lessThanOrEqual, file: file, line: line)
+    }
+    
+    @discardableResult
+    public func lessThanOrEqualToSuperview(_ file: String = #file, _ line: UInt = #line) -> ConstraintMakerEditable {
+        guard let other = self.description.item.superview else {
+            fatalError("Expected superview but found nil when attempting make constraint `lessThanOrEqualToSuperview`.")
+        }
+        return self.relatedTo(other, relation: .lessThanOr
