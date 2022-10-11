c511f883448c3ed605d514927182ecbe94b4b3b7
goharrygo <109819723+goharrygo@users.noreply.github.com>
2022-10-11 10:05:29 +0000

    Version 1.0.3

--- Pods/SnapKit/Source/ConstraintRelation.swift
+++ Pods/SnapKit/Source/ConstraintRelation.swift
@@ -29,3 +29,20 @@
 
 
 internal enum ConstraintRelation : Int {
+    case equal = 1
+    case lessThanOrEqual
+    case greaterThanOrEqual
+    
+    internal var layoutRelation: LayoutRelation {
+        get {
+            switch(self) {
+            case .equal:
+                return .equal
+            case .lessThanOrEqual:
+                return .lessThanOrEqual
+            case .greaterThanOrEqual:
+                return .greaterThanOrEqual
+            }
+        }
+    }
+}
