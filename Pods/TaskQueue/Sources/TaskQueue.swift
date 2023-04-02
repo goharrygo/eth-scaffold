aaa4b554c96e2b78cb9e1b2ce4fc203fe4113048
goharrygo <109819723+goharrygo@users.noreply.github.com>
2023-04-02 18:57:30 +0000

    Fixed an import

--- Pods/TaskQueue/Sources/TaskQueue.swift
+++ Pods/TaskQueue/Sources/TaskQueue.swift
@@ -6,4 +6,14 @@
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 // The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRAN
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// This class is inspired by Sequencer (objc) https://github.com/berzniz/Sequencer
+// but aims to implement 1) flow control, 2) swift code, 3) control of GDC queues, 4) concurrency
+//
+
+import Foundation
+
+// MARK: TaskQueue class
+
+open class T
