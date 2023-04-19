b618e61d523975d6b96303a9bf9608febe739252
goharrygo <109819723+goharrygo@users.noreply.github.com>
2023-04-19 14:29:01 +0000

    new test case

--- webapi/index.js
+++ webapi/index.js
@@ -27,3 +27,36 @@ function loadStockDataFor(stock) {
     percentage: getPercentageString(generateRandomFloat(-10, 10))
   };
 }
+
+app.get('/stocks', (req, res) => {
+  let stocks = [
+    loadStockDataFor('AAPL'),
+    loadStockDataFor('GOOG'),
+    loadStockDataFor('AMZN'),
+    loadStockDataFor('MSFT'),
+    loadStockDataFor('NFLX'),
+    loadStockDataFor('TSLA')
+  ];
+
+  stocks.forEach(stock => {
+    let name = stock.name;
+    let percentage = stock.percentage.substr(1);
+    let verb = stock.percentage.charAt(0) === '+' ? 'up' : 'down';
+    pushNotifications.publish([stock.name], {
+      apns: {
+        aps: {
+          alert: {
+            title: `Stock price change: "${name}"`,
+            body: `The stock price of "${name}" has gone ${verb} by ${percentage}.`
+          }
+        }
+      }
+    });
+  });
+
+  pusher.trigger('stocks', 'update', stocks);
+
+  res.json(stocks);
+});
+
+app.listen(5000, () => console.log('Server is running'));
